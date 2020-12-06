481d96d9efa0f496ed21c4157f589cc7f9f5a349
OwnOS <Owner2303@gmail.com>
2020-12-05 19:01:07 -0500

    Add the todo files

--- editdojo_project/urls.py
+++ editdojo_project/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hello.views import my_view
-
+from todo.views import  todo_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sayHello/', my_view),
+    path('todo/', todo_view),
 ]
